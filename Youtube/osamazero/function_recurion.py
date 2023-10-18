abfc4faf43527f79efcbd1e7a053f7122cf673bd
Mohamed Khalid <mohamedkhaledabdelwhap@gmail.com>
2023-10-18 21:20:36 +0300

    Function recurion

---
+++ Youtube/osamazero/function_recurion.py
@@ -0,0 +1,9 @@
+# Recurion Function From osama zero
+def cleanword(word):
+    if len(word) == 1:
+        return word
+    if word[0] == word[1]:
+        return cleanword(word[1:])
+    return word[0]+ cleanword(word[1:])
+
+print(cleanword("WWWoooorrrllllddd"))
